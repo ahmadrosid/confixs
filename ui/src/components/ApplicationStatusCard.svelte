5aa48a8ce5274ca027bda14fe32b7c15b68b4788
ahmadrosid <alahmadrosid@gmail.com>
2024-07-21 10:42:54 +0700

    chore: tidy up

--- ui/src/components/ApplicationStatusCard.svelte
+++ ui/src/components/ApplicationStatusCard.svelte
@@ -1,9 +1,21 @@
 <script>
   import { CheckCircle, XCircle } from "lucide-svelte";
   import RetroOutline from "./ui/RetroOutline.svelte";
+  import Button from "./ui/Button.svelte";
 
+  // These should be props or fetched from an API
   export let isNginxInstalled = true;
   export let isCertbotInstalled = false;
+
+  function handleNginxAction() {
+    // Add logic for Nginx action (e.g., installation, configuration)
+    console.log("Nginx action triggered");
+  }
+
+  function handleCertbotAction() {
+    // Add logic for Certbot action (e.g., installation, SSL certificate management)
+    console.log("Certbot action triggered");
+  }
 </script>
 
 <RetroOutline childClassName="p-0">
@@ -16,16 +28,22 @@
         <span class="font-semibold flex-grow">Nginx</span>
         {#if isNginxInstalled}
           <CheckCircle class="text-green-500" size={24} />
+          <Button on:click={handleNginxAction}>Configure</Button>
         {:else}
           <XCircle class="text-red-500" size={24} />
+          <Button on:click={handleNginxAction}>Install</Button>
         {/if}
       </li>
       <li class="flex items-center space-x-2">
         <span class="font-semibold flex-grow">Certbot</span>
         {#if isCertbotInstalled}
           <CheckCircle class="text-green-500" size={24} />
+          <Button on:click={handleCertbotAction}>Manage SSL</Button>
         {:else}
           <XCircle class="text-red-500" size={24} />
+          <Button on:click={handleCertbotAction} variant="danger">
+            Install
+          </Button>
         {/if}
       </li>
     </ul>
